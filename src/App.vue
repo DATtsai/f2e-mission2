9f15aa10a15c1bae0d535b6ea4260dd621ab32da
sdyou <sdyou@blueplanet.com.tw>
2023-11-14 01:35:42 +0800

    feat: 重新建vue3專案及安裝bpchart

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,125 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import D3ModuleContainer from '@bpchart/vue/components/D3ModuleContainer.vue'
+import { ChartColumnBar } from '@bpchart/d3-modules/chartColumn'
+
+const dataset = {
+  "data": [
+    {
+      "value": 55
+    },
+    {
+      "value": 150
+    },
+    {
+      "value": 45
+    },
+    {
+      "value": 2
+    },
+    {
+      "value": 3
+    },
+    {
+      "value": 30
+    }
+  ],
+  "xLabels": [
+    "心",
+    "讚",
+    "哈",
+    "怒",
+    "嗚",
+    "哇"
+  ],
+  "itemLabels": []
+}
+
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+      <D3ModuleContainer
+        :module="ChartColumnBar"
+        :dataset="dataset"
+      />
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
 nav {
-  padding: 30px;
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
