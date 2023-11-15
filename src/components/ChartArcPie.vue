b6a2e73f13fcb73c066d11adf012971e0f9a67e8
DAT <dat-1293@hotmail.com>
2023-11-15 23:14:31 +0800

    feat. chart demo

---
+++ src/components/ChartArcPie.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <D3ModuleContainer
+      :module="ChartArcPie"
+      :dataset="dataset"
+      :params="params"
+    />
+    選區票數說明：(需另外切版)
+    <ul>
+      <li>中山區</li>
+      <li>有效票數：141,880</li>
+      <li>無效票數：1,922</li>
+      <li>投票數：143,802</li>
+      <li>投票率：75.6097％</li>
+    </ul>
+  </div>
+</template>
+<script setup>
+import D3ModuleContainer from '@bpchart/vue/components/D3ModuleContainer.vue'
+import { ChartArcPie } from '@bpchart/d3-modules/chartArc'
+
+const dataset = {
+  data: [
+    {
+      "itemLabel": "宋楚瑜、余湘",
+      "value": 6367
+    },
+    {
+      "itemLabel": "韓國瑜、張善政",
+      "value": 56491
+    },
+    {
+      "itemLabel": "蔡英文、賴清德",
+      "value": 79022
+    },
+  ],
+  itemLabels: [
+      "宋楚瑜、余湘",
+      "韓國瑜、張善政",
+      "蔡英文、賴清德",
+  ],
+  arcLabels: [
+    "宋楚瑜、余湘",
+    "韓國瑜、張善政",
+    "蔡英文、賴清德",
+  ]
+}
+
+const params = {
+  "graphicArcPie": {
+    "outerMouseoverRadius": 1,
+    "outerRadius": 0.95,
+    "innerRadius": 0.7,
+    "padAngle": 0.02,
+    "cornerRadius": 0
+  },
+  "centerText": (e,t,a=1)=>"mouseover"===t?[e.arcLabel,e.datum.value]:["蔡英文、賴清德",Math.round(e.dataset.data.reduce((e,t)=>e+t.value,0)*a)],
+  "centerTextStyles": [
+    { 
+      "font-weight": "bold",
+      "text-anchor": "middle",
+      "pointer-events": "none",
+      "dominant-baseline": "middle",
+      "font-size": 20,
+      "fill": "#000"
+    },
+    {
+      "pointer-events": "none",
+      "dominant-baseline": "middle",
+      "text-anchor": "middle",
+      "font-size": 18,
+      "fill": "rgb(96, 98, 102)"
+    }
+  ],
+  "centerTextAttrs": [
+    {
+      "transform": "translate(0, -12)"
+    },
+    {
+      "transform": "translate(0, 48)"
+    }
+  ],
+  colors: ['#FF6310','#000095','#1B9431']
+}
+</script>
---
+++ src/components/ChartColumnTwoScalesBarGroupAndLine.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <D3ModuleContainer
+      :module="ChartColumnTwoScalesBarGroupAndLine"
+      :dataset="dataset"
+      :params="params"
+    />
+  </div>
+</template>
+<script setup>
+import D3ModuleContainer from '@bpchart/vue/components/D3ModuleContainer.vue'
+import { ChartColumnTwoScalesBarGroupAndLine } from '@bpchart/d3-modules/chartColumnTwoScales'
+
+const dataset = {
+  "y1Data": [
+  [
+    {
+      "value": 70769
+    },
+    {
+      "value": 14103
+    },
+    {
+      "value": 41075
+    },
+    {
+      "value": 55309
+    },
+  ],
+  [
+    {
+      "value": 685830
+    },
+    {
+      "value": 102725
+    },
+    {
+      "value": 339702
+    },
+    {
+      "value": 610896
+    },
+  ],
+  [
+    {
+      "value": 875854
+    },
+    {
+      "value": 144274
+    },
+    {
+      "value": 786471
+    },
+    {
+      "value": 1097621
+    },
+  ]],
+  "y2Data": [[
+    {
+      "value": 76.3098
+    },
+    {
+      "value": 75.7679
+    },
+    {
+      "value": 77.4424
+    },
+    {
+      "value": 76.5973
+    },
+  ],],
+  "xLabels": [
+    "台北市",
+    "新竹市",
+    "台南市",
+    "高雄市",
+  ],
+  "y1ItemLabels": [
+    "宋楚瑜、余湘",
+    "韓國瑜、張善政",
+    "蔡英文、賴清德",
+  ],
+  "y2ItemLabels": [
+    "得票率",
+  ],
+}
+
+const params = {
+  y1Colors: ['#FF6310','#000095','#1B9431']
+}
+</script>
