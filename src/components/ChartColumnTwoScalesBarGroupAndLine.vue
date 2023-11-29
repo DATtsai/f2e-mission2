c4a854e0b7de68d732c69c90426068008512b2ff
DAT <dat-1293@hotmail.com>
2023-11-29 20:18:03 +0800

    fix. candidate filter & tooltip

--- src/components/ChartColumnTwoScalesBarGroupAndLine.vue
+++ src/components/ChartColumnTwoScalesBarGroupAndLine.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <D3ModuleContainer
-      :key="key"
       :module="ChartColumnTwoScalesBarGroupAndLine"
       :dataset="dataset"
       :params="params"
@@ -23,13 +22,12 @@
   </div>
 </template>
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import D3ModuleContainer from '@bpchart/vue/components/D3ModuleContainer.vue'
 import { ChartColumnTwoScalesBarGroupAndLine } from '@bpchart/d3-modules/chartColumnTwoScales'
 
 const candidateColor = {1: '#FFAB6F', 2: '#63ABE9', 3: '#8EC48E'}
 const candidateLabel = {1: '宋楚瑜、余湘', 2: '韓國瑜、張善政', 3: '蔡英文、賴清德'}
-const key = ref(0)
 const params = ref({
   y1Colors: [],
   axisColumnTwoScales: {
@@ -51,19 +49,13 @@ const params = ref({
   },
   tooltipAside: {
     templateHtml: (d) => {
-      return `
-        得票數<br>${d.dataset.y1Data.map((i, index) => {
-          let name;
-          index === 0 ? name = '宋楚瑜、余湘 : ': false;
-          index === 1 ? name = '韓國瑜、張善政 : ': false;
-          index === 2 ? name = '蔡英文、賴清德 : ': false;
-          return `${ name + i[0].value } 票`
-        }).join("<br>")}
-      `
+      let str = d.xLabel + ' 得票數<br><hr>'
+      let index = d.xIndex
+      str += d.dataset.y1Data.map((item, ind) => d.dataset.y1ItemLabels[ind] + ' : ' + item[index].value).join('<br>')
+      return str
     }
   }
 })
-
 const dataset = ref({
   "y1Data": [],
   "y2Data": [],
@@ -74,23 +66,16 @@ const dataset = ref({
 const props = defineProps(['columnData']);
 const xlabel = [];
 
-const dataList = props.columnData;
-// console.log('data', dataList)
-
-dataList.forEach((item, index) => {
-  if(index === 0) {
-    dataset.value.y1Data = item.filter.map(element => {return [{ value: element.getBallot }]})
-    dataset.value.y1ItemLabels = item.filter.map(element => candidateLabel[element.candidateNo])
-    params.value.y1Colors = item.filter.map(element => candidateColor[element.candidateNo])
-  }
-  else{
-    item.filter.forEach((element, ind) => { dataset.value.y1Data[ind].push({ value: element.getBallot })})
-  }
-
-  // console.log('dataset', dataset.value)
+const dataList = computed(() => props.columnData)
+dataList.value.forEach(item => {
   dataset.value.xLabels.push(item.basic.label);
+  if(!params.value.y1Colors.length) params.value.y1Colors = item.filter.map(element => candidateColor[element.candidateNo])
+  if(!dataset.value.y1ItemLabels.length) dataset.value.y1ItemLabels = item.filter.map(element => candidateLabel[element.candidateNo])
+  item.filter.forEach((element, index) => { 
+    if(!dataset.value.y1Data[index]) dataset.value.y1Data[index] = []
+    dataset.value.y1Data[index].push({ value: element.getBallot })
+  })
 });
-// console.log('dataset', dataset)
 
 </script>
 
